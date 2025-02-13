c81f8f7615bf5df5dbee09d3377979f1e9ad131e
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2025-02-13 19:19:11 -0300

    Add files via upload

---
+++ src/main/java/com/marciobueno/aos/ex01helloworld/Ex01HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.marciobueno.aos.ex01helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01HelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marciobueno/aos/ex01helloworld/Ex01HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciobueno.aos.ex01helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
