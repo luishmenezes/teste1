a4c50d7ea426f6d919484076abd11a3807a63b39
Luis Henrique <luishpmenezes@hotmail.com>
2025-02-24 19:36:25 -0300

    go

--- src/main/java/com/marciobueno/aos/ex01helloworld/HelloController.java
+++ src/main/java/com/marciobueno/aos/ex01helloworld/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(@RequestParam String name) {
-        return "Hello " + name;
+
+        return "Hello Mestre supremo: " + name;
     }
 }
