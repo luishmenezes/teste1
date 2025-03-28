c966bfc2047e7b726f7bd94fe5324c5054c81ec7
Luis Henrique <luishpmenezes@hotmail.com>
2025-03-28 20:55:07 -0300

    add rest Controller

---
+++ src/main/java/com/marciobueno/aos/ex01helloworld/ApiController.java
@@ -0,0 +1,23 @@
+package com.marciobueno.aos.ex01helloworld;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/info")
+public class ApiController {
+
+    @GetMapping
+    public Map<String, Object> getInfo() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://api.exchangerate-api.com/v4/latest/USD";
+
+        return restTemplate.getForObject(url, Map.class);
+    }
+
+}
