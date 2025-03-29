01e22f3098ecc742048ca24dc19cc0536db5d3f5
Luis Henrique <luishpmenezes@hotmail.com>
2025-03-28 21:24:08 -0300

    att

--- src/main/java/com/marciobueno/aos/ex01helloworld/ApiController.java
+++ src/main/java/com/marciobueno/aos/ex01helloworld/ApiController.java
@@ -2,12 +2,14 @@ package com.marciobueno.aos.ex01helloworld;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/info")
 public class ApiController {
 
